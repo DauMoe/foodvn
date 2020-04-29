81b7318d79d1229bc01d9a306a9cc34d338f2984
DauMoe <DauMoe@users.noreply.github.com>
2020-04-29 16:00:53 +0700

    responsive edit

--- js/custom.js
+++ js/custom.js
@@ -8,8 +8,24 @@ let tip = document.getElementById('tip');
 let eating = document.getElementById('eating');
 let recipe = document.getElementById('recipes');
 let contact = document.getElementById('contact');
+let animateRecipes = document.querySelectorAll('.recipes');
+let html = document.querySelector('html');
+// let loading = document.querySelector('.loading');
 
 window.scrollTo(0, 0);
+new WOW({
+    offset: 250
+}).init();
+window.onload = () => {
+    // setInterval(() => {
+    //     loading.classList.add('wow', 'hinge');
+    // }, 1000);
+    
+    // setInterval(() => {
+    //     loading.style.display = 'none';
+    //     html.style.overflow = 'auto';
+    // }, 2000);
+}
 navLinkActive = (index)=> {
     for (let i=0; i<navLink.length; i++) {
         if (i == index) {
@@ -20,6 +36,14 @@ navLinkActive = (index)=> {
     }
 }
 
+for (let i=0; i<animateRecipes.length; i++) {
+    animateRecipes[i].classList.add('wow');
+    if (i%2 == 0) {
+        animateRecipes[i].classList.add('fadeInLeft');
+    } else {
+        animateRecipes[i].classList.add('fadeInRight');
+    }
+}
 // $('.carousel').carousel({
 //     interval:  2500
 // })
