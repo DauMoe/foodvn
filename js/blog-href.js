b796be113354f41819b7ff071062eee78f9c0fc1
DauMoe <DauMoe@users.noreply.github.com>
2020-05-03 14:53:42 +0700

    blog

---
+++ js/blog-href.js
@@ -0,0 +1,4 @@
+const baseURL = window.location.protocol + '//' + window.location.host;
+let homepage = document.querySelector('#homepage');
+
+homepage.href = baseURL;
---
+++ js/homepage-href.js
@@ -0,0 +1,4 @@
+const baseURL = window.location.protocol + '//' + window.location.host;
+let cupcake_blog = document.querySelector('#cupcake_blog');
+
+cupcake_blog.href = baseURL + '/blog.html';
